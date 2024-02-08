d1d36a8c5a948cdab7000ecd3e171a1ba70b81a1
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2024-02-08 18:42:54 +0530

    Update SessionManager.java

--- app/src/main/java/com/example/registrationapp/SessionManager.java
+++ app/src/main/java/com/example/registrationapp/SessionManager.java
@@ -1,43 +1,64 @@
 package com.example.registrationapp;
 
+
 import android.content.Context;
 import android.content.SharedPreferences;
-
-
 public class SessionManager {
-    private static final String PREF_NAME = "UserSession";
-    private static final String KEY_NAME = "name"; // Change key to save user's name
-    private static final String KEY_EMAIL = "email";
-    private SharedPreferences pref;
-    private SharedPreferences.Editor editor;
-    private Context context;
-    private int PRIVATE_MODE = 0;
-
-    public SessionManager(Context context) {
-        this.context = context;
-        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
-        editor = pref.edit();
+    Context context;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+    private final String PREF_FILE_NAME = "registerApp";
+    private final String KEY_IF_LOGGED_IN = "key_session_if_logged_in";
+    private final String KEY_NAME = "key_session_name";
+    private final String KEY_EMAIL = "key_session_email";
+    private final String KEY_CONATCT = "key_session_contact";
+    private  final int PRIVATE_MODE =0;
+    public boolean checkSession ()
+    {
+        if(sp.contains(KEY_IF_LOGGED_IN))
+            return true;
+        else
+            return false;
     }
-
-    public void saveName(String name) { // Modify method to save user's name
-        editor.putString(KEY_NAME, name);
-        editor.apply();
+    public SessionManager(Context context){
+        this.context =context;
+        sp=context.getSharedPreferences(PREF_FILE_NAME,PRIVATE_MODE);
+        editor= sp.edit();
     }
-
-    public String getName() { // Modify method to get user's name
-        return pref.getString(KEY_NAME, null);
+    public void createSession(String username , String email , String contact)
+    {
+        editor.putString(KEY_NAME , username);
+        editor.putString(KEY_EMAIL , email);
+        editor.putString(KEY_CONATCT , contact);
+        editor.putBoolean(KEY_IF_LOGGED_IN, true);
+        editor.commit();
     }
-
-    public void saveEmail(String email) {
-        editor.putString(KEY_EMAIL, email);
-        editor.apply();
+    public String getSessionDetails(String key)
+    {
+        String value = sp.getString(key,null);
+        return value;
     }
-    public String getEmail() {
-        return pref.getString(KEY_EMAIL, null);
-    }
-
-    public void clearSession() {
+    public void clearSession()
+    {
         editor.clear();
+        editor.commit();
+    }
+    public String saveName(String name)
+    {
+        editor.putString(KEY_NAME,name);
         editor.apply();
+        return name;
+    }
+    public String saveEmail (String email)
+    {
+        editor.putString(KEY_EMAIL,email);
+        editor.apply();
+        return email;
+    }
+    public String saveContact(String contact)
+    {
+        editor.putString(KEY_CONATCT,contact);
+        editor.apply();
+        return contact;
     }
 }
