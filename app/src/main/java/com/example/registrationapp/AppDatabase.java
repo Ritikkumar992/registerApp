249e6ef44aa009efc16767761fc8e51e8f5087aa
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2024-02-08 18:41:19 +0530

    Update AppDatabase.java

--- app/src/main/java/com/example/registrationapp/AppDatabase.java
+++ app/src/main/java/com/example/registrationapp/AppDatabase.java
@@ -6,6 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+ // App database created.
 @Database(entities = {User.class},version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
@@ -28,5 +29,6 @@ public abstract class AppDatabase extends RoomDatabase {
     }
 
     public abstract UserDao userDAO();
+   
 
 }
