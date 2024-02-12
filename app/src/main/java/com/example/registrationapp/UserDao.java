7bdd739513064367d957af508652999748e33438
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2024-02-12 22:27:57 +0530

    Update UserDao.java

--- app/src/main/java/com/example/registrationapp/UserDao.java
+++ app/src/main/java/com/example/registrationapp/UserDao.java
@@ -6,12 +6,15 @@ import androidx.room.Query;
 @Dao
 public interface UserDao {
 
+    // delete query.
     @Query("DELETE FROM usersTable")
     void deleteAllUsers();
 
+    // insert query.
     @Query("INSERT INTO usersTable(name,userEmail,password,contact) VALUES(:fullName,:userEmail,:password,:contact)")
     void insertUser(String fullName, String userEmail, String password, String contact);
 
+    // select query 
     @Query("SELECT * FROM usersTable WHERE userEmail =:userEmail AND password =:password")
     User getUserByUserEmailAndPassword(String userEmail,String password);
 
@@ -21,7 +24,8 @@ public interface UserDao {
 
     @Query("SELECT * FROM usersTable WHERE userEmail = :userEmail")
     User getUserEmail(String userEmail);
-
+    
+    // update query
     @Query("UPDATE usersTable SET name = :newName WHERE userEmail = :userEmail")
     void updateUserName(String newName, String userEmail);
 
