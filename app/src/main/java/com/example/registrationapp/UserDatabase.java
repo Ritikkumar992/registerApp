4474d29be1760c3ade8f34363c8d5133b3fce6e4
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2024-02-12 21:23:23 +0530

    Update UserDatabase.java

--- app/src/main/java/com/example/registrationapp/UserDatabase.java
+++ app/src/main/java/com/example/registrationapp/UserDatabase.java
@@ -1,34 +1,26 @@
 package com.example.registrationapp;
-
 import android.content.Context;
-
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
  // App database created.
 @Database(entities = {User.class},version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-
-    public static AppDatabase database;
-
+public abstract class UserDatabase extends RoomDatabase {
+    public static UserDatabase database;
     private static final String databaseName = "UserDb";
-
-
-    public synchronized static AppDatabase getInstance(Context context)
-    {
-        if(database == null)
-        {
-            database = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class,databaseName)
+ 
+    // Method to get an instance of the database
+    public synchronized static UserDatabase getInstance(Context context){
+        if(database == null){
+            database = Room.databaseBuilder(context.getApplicationContext(),// Building the database using Room database builder
+                            UserDatabase.class,databaseName)
                     .allowMainThreadQueries()
                     .fallbackToDestructiveMigration()
                     .build();
         }
         return database;
     }
-
+    // Abstract method to provide access to UserDao
     public abstract UserDao userDAO();
-   
-
 }
